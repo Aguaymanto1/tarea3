280c267057b2e35028857bda67d3874f497c3824
Jesus <jesusandredelacruzmantilla@gmail.com>
2025-08-10 16:17:55 -0500

    model mascota listo

---
+++ Models/Mascota.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+namespace tarea3.Models;
+
+public class Mascota
+{
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public String nombre { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public String tipo { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public String raza { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public String edad { get; set; }
+     [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public String contacto { get; set; }
+}
