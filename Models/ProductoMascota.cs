8bc0bc11bbb73cb932ef01167d8ffcf5bca5c9bf
Jesus <jesusandredelacruzmantilla@gmail.com>
2025-08-10 17:30:20 -0500

    controller termiando

---
+++ Models/ProductoMascota.cs
@@ -0,0 +1,11 @@
+namespace tarea3.Models;
+
+public class ProductoMascota
+{
+
+        public string Nombre { get; set; }
+        public string Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public string Descripcion { get; set; }
+
+}
